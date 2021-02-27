180357bf9769be355fdf96d61626f7f8be8217ab
Raymond Hunce <hunce18c@gmail.com>
2021-02-27 16:32:21 -0500

    Implement LocalStrategy Authentication with Passport

--- config/passport.js
+++ config/passport.js
@@ -1,3 +1,35 @@
 const LocalStrategy = require('passport-local').Strategy;
 const bcrypt = require('bcrypt');
 const User = require("../models/user");
+
+module.exports = function(passport) {
+  passport.use(
+    new LocalStrategy({ usernameField : 'email' }, (email, password, done) => {
+      // Match user
+      User.findOne({ email: email })
+      .then((user) => {
+        if (!user) {
+          return done(null, false, { message: 'That email is not registered.' });
+        }
+        // Match pass
+        bcrypt.compare(password, user.password,(err, isMatch) => {
+          if(err) { throw err; }
+          if(isMatch) {
+            return done(null, user);
+          } else {
+            return done(null, false, { message: 'Password incorrect.' });
+          }
+        })
+      })
+      .catch((err) => { console.log(err); });
+    })
+  )
+  passport.serializeUser(function(user, done) {
+    done(null, user.id);
+  });
+  passport.deserializeUser(function(id, done) {
+    User.findById(id, function(err, user) {
+      done(err, user);
+    });
+  });
+};
